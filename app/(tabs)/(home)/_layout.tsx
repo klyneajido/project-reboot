f816aeaa30cdc487b5525f2ac6d46cf45b23a9d8
Andriq Klyne Ajido <ajido.andriqklyne@gmail.com>
2025-05-27 13:53:28 +0800

    refactor: removed settings folder and its initial tabs route, added Icons

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -2,19 +2,8 @@ import { Stack } from 'expo-router';
 
 export default function HomeLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#333333',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
+    <Stack screenOptions={{headerShown:false}}>
       <Stack.Screen name="index" />
-      <Stack.Screen name="AMTodoItem" />
-      <Stack.Screen name="PMTodoItem" />
     </Stack>
   );
 }
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,26 +1,19 @@
 // app/(tabs)/_layout.tsx
+import { FontAwesome } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: '#f4511e',
-      }}
-    >
+    <Tabs>
       <Tabs.Screen
         name="(home)"
         options={{
           title: 'Home',
           headerShown: false,
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name='home' color={color}/>
         }}
       />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: 'Settings',
-        }}
-      />
+      
     </Tabs>
   );
 }
